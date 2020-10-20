785bc6c741840cda3f244300c02031cb6499971d
Micha Gorelick <mynameisfiber@gmail.com>
2020-10-20 18:46:05 +0200

    Bump version: 0.9.0 → 0.9.1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt") as f:
 
 setup(
     name="followthemoney-predict",
-    version="0.9.0",
+    version="0.9.1",
     author="Organized Crime and Corruption Reporting Project",
     author_email="data@occrp.org",
     url="https://github.com/alephdata/followthemoney-predict/",
